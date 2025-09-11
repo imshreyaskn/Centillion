6cec2e11bfeb3311b089aa9fb28db8a637f501fc
Shreyas <oyedrizzle@gmail.com>
2025-09-11 14:30:44 +0530

    Initial commit

---
+++ GoLangIntro/Loops/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	//While Loop
+
+	x := 0
+
+	for x < 5 {
+		fmt.Println(x)
+		x++
+	}
+
+	//For Loop
+	for y := 0; y < 5; y++ {
+		fmt.Println(y)
+	}
+
+	//For-Range Loop
+
+	names := []string{"mario", "yoshi", "peach"}
+
+	for index, value := range names {
+		fmt.Printf("The Value: %v. The Index: %v \n", value, index)
+	}
+
+	// Use underscore '_' to discard unwanted variable
+
+	//Slice Modification using For-Range loop
+
+	for indx, _ := range names {
+		names[indx] = "None"
+	}
+
+	//Infinte loop
+
+	z := 0
+
+	for {
+		if z > 5 {
+			break
+		}
+		fmt.Println(z)
+		z++
+	}
+
+}
